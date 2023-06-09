1c4ebec81ee32326746812dfe2e91b36ca64fc94
OneAndZero24 <oneandzero24@gmail.com>
2023-06-09 19:57:58 +0200

    fix: stoopid mistaek

--- gwnmo/train.py
+++ gwnmo/train.py
@@ -18,7 +18,7 @@ def test(module: ModuleABC, test_loader):
 
     with torch.no_grad():
         for i, (X, y) in enumerate(test_loader):
-            X.to(device), y.to(device)
+            X, y = X.to(device), y.to(device)
             _, preds, err = module.training_step((X, y), i)
             test_error += err
             test_accuracy += accuracy(preds, y)
