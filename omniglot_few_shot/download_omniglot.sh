ece00445cdd5c420d2f400fb13d5817d02f27e76
Mateusz Rajski <mateusz.rajski@swmansion.com>
2023-12-15 15:40:04 +0100

    Add base_path

--- omniglot_few_shot/download_omniglot.sh
+++ omniglot_few_shot/download_omniglot.sh
@@ -3,7 +3,8 @@ curl -o train.txt https://raw.githubusercontent.com/jakesnell/prototypical-netwo
 curl -o val.txt https://raw.githubusercontent.com/jakesnell/prototypical-networks/master/data/omniglot/splits/vinyals/val.txt
 curl -o test.txt https://raw.githubusercontent.com/jakesnell/prototypical-networks/master/data/omniglot/splits/vinyals/test.txt
 
-DATADIR=./images
+# OMNIGLOT_PATH
+DATADIR="${OMNIGLOT_PATH}/images"
 mkdir -p $DATADIR
 curl -o images_background.zip https://github.com/brendenlake/omniglot/blob/master/python/images_background.zip?raw=true -L
 curl -o images_evaluation.zip https://github.com/brendenlake/omniglot/blob/master/python/images_evaluation.zip?raw=true -L
@@ -14,6 +15,6 @@ mv $DATADIR/images_evaluation/* $DATADIR/
 rmdir $DATADIR/images_background
 rmdir $DATADIR/images_evaluation
 
-python rot_omniglot.py
-python write_omniglot_filelist.py
-python write_cross_char_base_filelist.py
+python "${OMNIGLOT_PATH}/rot_omniglot.py"
+python "${OMNIGLOT_PATH}/write_omniglot_filelist.py"
+python "${OMNIGLOT_PATH}/write_cross_char_base_filelist.py"
