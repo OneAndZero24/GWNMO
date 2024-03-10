f61457d18c58957ee13ee1d25e8013acdbbd7897
OneAndZero24 <oneandzero24@gmail.com>
2024-03-10 22:33:12 +0100

    test: smaller body

--- gwnmo/__init__.py
+++ gwnmo/__init__.py
@@ -1,1 +1,1 @@
-__version__ = "v2.18.0off3"
+__version__ = "v2.18.0off4"
--- gwnmo/models/body.py
+++ gwnmo/models/body.py
@@ -15,11 +15,7 @@ class Body(nn.Module):
 
         self.seq = nn.Sequential()
         self.seq.append(nn.BatchNorm1d(12544))
-        self.seq.append(nn.Linear(12544, 4096))
-        self.seq.append(nn.ReLU())
-        self.seq.append(nn.BatchNorm1d(4096))
-        self.seq.append(nn.Linear(4096, 1024))
-        self.seq.append(nn.ReLU())
+        self.seq.append(nn.Linear(12544, 1024))
 
     def forward(self, x: torch.Tensor):
         return self.seq(x)
